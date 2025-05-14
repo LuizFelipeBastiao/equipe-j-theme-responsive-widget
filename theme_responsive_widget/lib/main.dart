185429abf4c9a432bb3a1f6a437fc572eb7351dc
Junior Sfredo <gilvan.sfr@gmail.com>
2025-05-14 10:32:35 -0300

    Resolving merge in main

--- theme_responsive_widget/lib/main.dart
+++ theme_responsive_widget/lib/main.dart
@@ -4,21 +4,3 @@ import 'package:theme_responsive_widget/app.dart';
 void main() {
   runApp(MyApp());
 }
-
-
-<<<<<<< HEAD
-=======
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-      
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const Paginainicial(),
-    );
-  }
-}
->>>>>>> a742d57615c37253eac00c54c2f05a5248257065
